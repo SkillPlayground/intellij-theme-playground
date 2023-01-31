52080a10b3400bc001927769dd81fd955601a009
GitHub Action <action@github.com>
2023-01-31 09:14:49 +0000

    Template cleanup

--- src/main/kotlin/com/github/javiersegoviacordoba/intellijthemeplayground/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/javiersegoviacordoba/intellijthemeplayground/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.javiersegoviacordoba.intellijthemeplayground.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.javiersegoviacordoba.intellijthemeplayground.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/javiersegoviacordoba/intellijthemeplayground/services/MyProjectService.kt
+++ src/main/kotlin/com/github/javiersegoviacordoba/intellijthemeplayground/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.javiersegoviacordoba.intellijthemeplayground.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.javiersegoviacordoba.intellijthemeplayground.MyBundle
 
 class MyProjectService(project: Project) {
 
